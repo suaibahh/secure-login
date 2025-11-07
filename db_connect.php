3976f3a9a5f3e55df5b83495a9cdbe412701d138
azmibasri <azmibasri391@gmail.com>
2025-11-07 11:30:14 +0700

    perbaiki json vecel

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,23 @@
 <?php
-$servername = getenv('DB_HOST') ?: 'localhost';
-$username = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASS') ?: '';
-$database = getenv('DB_NAME') ?: 'db_auth';
-$port = getenv('DB_PORT') ?: 3306;
+// Ambil variabel dari Vercel
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$database = getenv('DB_NAME');
+$port = (int)getenv('DB_PORT');
 
-$conn = new mysqli($servername, $username, $password, $database, $port);
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+
+$conn = mysqli_init();
+
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+if (!mysqli_real_connect($conn, $servername, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+
+$conn->set_charset("utf8mb4");
+
+
 ?>
